e1f1964dae56009a1b8c14dbdb649e4bccc3f917
WasyihunS <bywasyihun@gmail.com>
2023-11-28 01:39:49 -0800

    Adding a file

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello world")
+from src.loader import SlackDataLoader
+from src.utils import *
+
+# Use SlackDataLoader to load data from Slack
+data = SlackDataLoader.load_data()
+
+# Use functions from src/utils.py to analyze data
